96ae44d0a0d717cbf25f1984fc66856418adf606
Enzo Figlioli <enzofiglioli.p@gmail.com>
2025-01-14 13:18:28 -0300

    agrega controlador de cierre de sesión y ruta correspondiente

--- app/controllers/usuarioController.js
+++ app/controllers/usuarioController.js
@@ -121,3 +121,21 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ msg: "Error al iniciar sesión" });
   }
 };
+
+
+export const logout = (req, res) => {
+  try {
+    const session = req.cookies.token;
+    if (!session) {
+      return res.status(404).json({ msg: "No existe una sesión activa" });
+    }
+
+    // Si existe la sesión, borrar la cookie
+    res.clearCookie("token");
+
+    return res.status(200).json({ msg: "Sesión cerrada correctamente" });
+  } catch (err) {
+    console.error("Error al cerrar sesión:", err.message);
+    res.status(500).json({ msg: "Error al cerrar sesión" });
+  }
+};
--- app/routes/usuarios.js
+++ app/routes/usuarios.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {crearUsuario, loginUser} from "../controllers/usuarioController.js";
-
+import {crearUsuario, loginUser, logout} from "../controllers/usuarioController.js";
+import {verifyToken} from "../middleware/auth.js"
 const route = express.Router();
 
 route.post("/", crearUsuario);
 route.post("/login", loginUser);
+route.get("/logout", verifyToken, logout);
 
 export default route;
